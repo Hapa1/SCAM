e4cdda34206cdb8d5668c4724c129a624d40d4cb
Gardener847 <oniabiel205@yahoo.com>
2017-05-07 16:40:10 -0700

    Add files via upload

---
+++ schedule/upload.updateSchedule/uploadSchedule.js
@@ -0,0 +1,16 @@
+var clickTimes = 0;
+function addAct() {
+	clickTimes ++;
+	if (clickTimes === 8){
+		var parent = document.getElementById("agenda");
+		var child = document.getElementById("addButton");
+		parent.removeChild(child);
+		return;
+	}
+	else{
+		var firstClass = document.getElementById("Act").innerHTML;
+	    var addClass = document.getElementById("addClass" + clickTimes).innerHTML;
+		addClass = firstClass;
+		document.getElementById("addClass" + clickTimes).innerHTML = addClass;
+	}
+};
