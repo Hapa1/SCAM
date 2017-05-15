e1e2e9369c11672756299ee51d05d24f71650060
Gardener847 <oniabiel205@yahoo.com>
2017-05-15 15:42:45 -0700

    Update uploadSchedule.js

--- schedule/upload.updateSchedule/uploadSchedule.js
+++ schedule/upload.updateSchedule/uploadSchedule.js
@@ -1,13 +1,13 @@
 var clickTimes = 0;
 function addAct() {
 	clickTimes ++;
-	if (clickTimes === 8){
+	if (clickTimes === 8) {
 		var parent = document.getElementById("agenda");
 		var child = document.getElementById("addButton");
 		parent.removeChild(child);
 		return;
 	}
-	else{
+	else {
 		var firstClass = document.getElementById("Act").innerHTML;
 	    var addClass = document.getElementById("addClass" + clickTimes).innerHTML;
 		addClass = firstClass;
